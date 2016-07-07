93af39f686435a18a0c284399501f38813d26bb9
Christopher Cliff <chris@percolate.com>
2016-07-07 14:30:22 -0500

    First commit.

---
+++ webpack.config.test.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack')
+
+module.exports = {
+    context: __dirname,
+    devtool: 'eval',
+    entry: {
+        index: './__test__/createReducer.spec.js',
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('test'),
+            },
+        }),
+    ],
+    resolve: {
+        root: __dirname,
+    },
+}
